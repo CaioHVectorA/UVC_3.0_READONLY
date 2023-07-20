3f429bb28a57eb83857fd84617733857a57ff034
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-07-20 01:19:37 +0000

    Update Full.ts

--- src/modules/Routes/Full.ts
+++ src/modules/Routes/Full.ts
@@ -2,6 +2,7 @@ import Series from "../Data/Series";
 import DataSolos from "../Data/Solo";
 import { Router } from "express";
 import shuffle from "../../utilities/shuffleArray";
+import Chars from "../Data/Char";
 
 export const FullRoute = Router();
 
@@ -14,4 +15,9 @@ FullRoute.get("/:ref", (req, res) => {
   const found = arr.find((item) => item.Ref === req.params.ref);
   if (!found) return res.status(404);
   return res.json(found);
+});
+
+FullRoute.get("/searchdata", (req, res) => {
+  //const data = ;
+  res.json(shuffle([...DataSolos, ...Series, ...Chars]));
 });
